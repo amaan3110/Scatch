d1eeb39a035d3f9537fb9f19b3a57e2521a161ba
amaan3110 <amaan03121@gmail.com>
2024-06-04 18:29:51 +0530

    update authRoute.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,12 @@ const { Register, Login, Logout } = require('../controllers/authController');
 
 
 router.get('/', (req, res) => {
-    res.render("index", { messages: req.flash() });
+    if (req.cookies.token) {
+        return res.redirect('/user');
+    } else {
+        return res.render("index", { messages: req.flash() });
+    }
+
 });
 router.post('/login', Login);
 router.post('/register', Register);
